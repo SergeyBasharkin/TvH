dfd9c75579d2ddf5ac5a105775317680d2102768
SergeyBasharkin <rock-n-rolllll@bk.ru>
2016-09-24 14:44:57 +0300

    config DB

--- src/main/java/com/klopblop/mvc/controllers/HelloController.java
+++ src/main/java/com/klopblop/mvc/controllers/HelloController.java
@@ -1,7 +1,6 @@
 package com.klopblop.mvc.controllers;
 
 import com.klopblop.mvc.Repositories.UserDao;
-import com.klopblop.mvc.Services.UserService;
 import com.klopblop.mvc.models.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
